e57518b1a8595b2eefeacbb2676cc77d254da8f9
jbm <837639305@qq.com>
2023-09-06 14:18:05 +0800

    update

--- Invokeai/entrypoint.sh
+++ Invokeai/entrypoint.sh
@@ -4,7 +4,7 @@ set -Eeuo pipefail
 
 declare -A MOUNTS
 
-mkdir -p ${CONFIG_DIR} ${ROOT}/configs/stable-diffusion/
+# mkdir -p ${CONFIG_DIR} ${ROOT}/configs/stable-diffusion/
 
 # cache
 MOUNTS["/root/.cache"]=/data/.cache/
@@ -13,12 +13,12 @@ MOUNTS["/root/.cache"]=/data/.cache/
 rm -rf ${HF_HOME}/diffusers
 
 # ui specific
-MOUNTS["${ROOT}/models/codeformer"]=/data/models/Codeformer/
-MOUNTS["${ROOT}/models/gfpgan/GFPGANv1.4.pth"]=/data/models/GFPGAN/GFPGANv1.4.pth
-MOUNTS["${ROOT}/models/gfpgan/weights"]=/data/models/GFPGAN/
-MOUNTS["${ROOT}/models/realesrgan"]=/data/models/RealESRGAN/
+MOUNTS["${INVOKEAI_ROOT}/models/codeformer"]=/data/models/Codeformer/
+MOUNTS["${INVOKEAI_ROOT}/models/gfpgan/GFPGANv1.4.pth"]=/data/models/GFPGAN/GFPGANv1.4.pth
+MOUNTS["${INVOKEAI_ROOT}/models/gfpgan/weights"]=/data/models/GFPGAN/
+MOUNTS["${INVOKEAI_ROOT}/models/realesrgan"]=/data/models/RealESRGAN/
 
-MOUNTS["${ROOT}/models/ldm"]=/data/.cache/invoke/ldm/
+MOUNTS["${INVOKEAI_ROOT}/models/ldm"]=/data/.cache/invoke/ldm/
 
 # hacks
 
@@ -36,10 +36,53 @@ for to_path in "${!MOUNTS[@]}"; do
   echo Mounted $(basename "${from_path}")
 done
 
+# USER_ID=${CONTAINER_UID:-1000}
+# USER=invoke
+# usermod -u ${USER_ID} ${USER} 1>/dev/null
+
+configure() {
+    # Configure the runtime directory
+    if [[ -f ${INVOKEAI_ROOT}/invokeai.yaml ]]; then
+        echo "${INVOKEAI_ROOT}/invokeai.yaml exists. InvokeAI is already configured."
+        echo "To reconfigure InvokeAI, delete the above file."
+        echo "======================================================================"
+    else
+        mkdir -p "${INVOKEAI_ROOT}"
+        # chown --recursive ${USER} "${INVOKEAI_ROOT}"
+        invokeai-configure --yes --default_only
+    fi
+}
+
+## Skip attempting to configure.
+## Must be passed first, before any other args.
+# if [[ $1 != "--no-configure" ]]; then
+#     configure
+# else
+#     shift
+# fi
+
+
 if "${PRELOAD}" == "true"; then
-  set -Eeuo pipefail
-  invokeai-configure --root ${ROOT} --yes
-  cp ${ROOT}/configs/models.yaml ${CONFIG_DIR}/models.yaml
+  configure
 fi
+
+### Set the $PUBLIC_KEY env var to enable SSH access.
+# We do not install openssh-server in the image by default to avoid bloat.
+# but it is useful to have the full SSH server e.g. on Runpod.
+# (use SCP to copy files to/from the image, etc)
+if [[ -v "PUBLIC_KEY" ]] && [[ ! -d "${HOME}/.ssh" ]]; then
+    apt-get update
+    apt-get install -y openssh-server
+    pushd "$HOME"
+    mkdir -p .ssh
+    echo "${PUBLIC_KEY}" > .ssh/authorized_keys
+    chmod -R 700 .ssh
+    popd
+    service ssh start
+fi
+
+
+cd "${INVOKEAI_ROOT}"
+
 
 exec "$@"
